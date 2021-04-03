fb0f95ded72b0fc0a762b9c5f243ad9a98620353
galacticBlack <bekzodjontojiboyev8@gmail.com>
2021-04-03 15:38:16 +0500

    adding new status

--- src/staffs/staff.controller.js
+++ src/staffs/staff.controller.js
@@ -19,6 +19,7 @@ exports.create = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the Staffs.",
+        status: "200 Status from Staff Page",
       });
     });
 };
